7114d05ebd8cf6b05618a3b41dc089db3d5ef8d8
DavidcD8 <91196677+DavidcD8@users.noreply.github.com>
2022-03-11 19:44:51 +0000

    Update ItemsList.java

--- ItemsList.java
+++ ItemsList.java
@@ -1,9 +1,7 @@
  
 import java.util.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+ 
+ 
 import java.util.Scanner;
 
 public class ItemsList {
